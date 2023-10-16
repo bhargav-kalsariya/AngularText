3ff230474239ec077e5762ede1d87634e76eb29c
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2023-10-16 18:07:50 +0530

    built-in pipes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angularTest';
+  Bday = new Date();
+  price = 1234.35442;
+  object = { name: 'random', age: 30 };
+  numberValue = 3.1335444;
+  percentValue = 0.75;
 }
