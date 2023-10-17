ab55a43a7241d6e74b643f078d8b87635033cc64
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2023-10-17 16:04:46 +0530

    custom directive done

--- src/app/directive/in-build.directive.ts
+++ src/app/directive/in-build.directive.ts
@@ -1,10 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[appInBuild]',
 })
-export class InBuildDirective {
-  constructor(private er: ElementRef) {
-    this.er.nativeElement.style.backgroundColor = 'red';
+export class InBuildDirective implements OnChanges {
+  constructor(private er: ElementRef) {}
+  @HostListener('window:keyup') ngOnChanges(event: any) {
+    let count = this.er.nativeElement.value.length;
+    if (count < 5) {
+      this.er.nativeElement.style.backgroundColor = 'red';
+    } else if (count >= 5 && count <= 10) {
+      this.er.nativeElement.style.backgroundColor = 'green';
+    } else {
+      this.er.nativeElement.style.backgroundColor = 'purple';
+    }
   }
 }
