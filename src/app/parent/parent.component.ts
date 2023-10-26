8aaed332d3fddf9a7604c60319cec44fbc50f3fb
bhargav_kalsriya <kalsariyabhargav187@gmail.com>
2023-10-26 18:22:47 +0530

    create an example using BehaviourSubject

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharingService } from '../sharing.service';
 
 @Component({
   selector: 'app-parent',
@@ -11,12 +12,22 @@ export class ParentComponent {
   parentcounter = 0;
   randomNumber = 0;
 
+  constructor(private sharingData: SharingService) { }
+
   generateRandomNumber() {
     this.randomNumber = Math.floor(Math.random() * 100);
     console.log(this.randomNumber);
   }
   clickHandler(value: any) {
     this.parentcounter = value;
+  }
+  sharingDataToOthers() {
+    this.sharingData.shareData.next([
+      { name: 'random', age: 12 },
+      { name: 'random1', age: 13 },
+      { name: 'random2', age: 14 },
+      { name: 'random3', age: 15 },
+    ]);
   }
 
 }
