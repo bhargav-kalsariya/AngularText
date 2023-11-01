e9913c617b9ba99d89e7678539f0de2f67691f40
bhargav_kalsriya <kalsariyabhargav187@gmail.com>
2023-11-01 12:11:31 +0530

    error handling with condition

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -4,38 +4,38 @@ import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
-
   mainForm!: FormGroup;
+  submitted = false;
 
-  countries = ['Select a country', 'India', 'China', 'Germany'];
+  countries = ['India', 'China', 'Germany'];
   states: { [key: string]: string[] } = {
-    'India': ['Gujarat', 'Maharashtra', 'Delhi'],
-    'China': ['Hainan', 'Sichuan', 'Yunnan'],
-    'Germany': ['Berlin', 'Bavaria', 'Saxony']
+    India: ['Gujarat', 'Maharashtra', 'Delhi'],
+    China: ['Hainan', 'Sichuan', 'Yunnan'],
+    Germany: ['Berlin', 'Bavaria', 'Saxony'],
   };
   cities: { [key: string]: string[] } = {
-    'Gujarat': ['Ahmedabad', 'Surat', 'Vadodara'],
-    'Maharashtra': ['Mumbai', 'Pune', 'Nagpur'],
-    'Delhi': ['New Delhi', 'Noida', 'Gurugram'],
-    'Hainan': ['Haikou', 'Sanya', 'Wenchang'],
-    'Sichuan': ['Chengdu', 'Chongqing', 'Leshan'],
-    'Yunnan': ['Kunming', 'Lijiang', 'Dali'],
-    'Berlin': ['Berlin City Center', 'Charlottenburg', 'Prenzlauer Berg'],
-    'Bavaria': ['Munich', 'Nuremberg', 'Augsburg'],
-    'Saxony': ['Dresden', 'Leipzig', 'Chemnitz']
+    Gujarat: ['Ahmedabad', 'Surat', 'Vadodara'],
+    Maharashtra: ['Mumbai', 'Pune', 'Nagpur'],
+    Delhi: ['New Delhi', 'Noida', 'Gurugram'],
+    Hainan: ['Haikou', 'Sanya', 'Wenchang'],
+    Sichuan: ['Chengdu', 'Chongqing', 'Leshan'],
+    Yunnan: ['Kunming', 'Lijiang', 'Dali'],
+    Berlin: ['Berlin City Center', 'Charlottenburg', 'Prenzlauer Berg'],
+    Bavaria: ['Munich', 'Nuremberg', 'Augsburg'],
+    Saxony: ['Dresden', 'Leipzig', 'Chemnitz'],
   };
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.mainForm = this.formBuilder.group({
       id: ['', [Validators.required]],
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      addresses: this.formBuilder.array([this.createAddress()])
+      addresses: this.formBuilder.array([this.createAddress()]),
     });
   }
 
@@ -43,10 +43,10 @@ export class ReactiveFormComponent implements OnInit {
     return this.formBuilder.group({
       address1: ['', Validators.required],
       address2: [''],
-      landmark: [''],
+      landmark: ['', Validators.required],
       country: ['', Validators.required],
       state: ['', Validators.required],
-      city: ['', Validators.required]
+      city: ['', Validators.required],
     });
   }
 
@@ -63,25 +63,18 @@ export class ReactiveFormComponent implements OnInit {
   }
 
   onSubmit() {
+    this.submitted = true;
+
     if (this.mainForm.valid) {
       console.log('Form Data:', this.mainForm.value);
       this.mainForm.reset();
+      this.submitted = false;
     }
   }
-  triggerErrorMessages() {
-    for (const key in this.mainForm.controls) {
-      if (this.mainForm.controls[key] instanceof FormGroup) {
-        const formGroup = this.mainForm.controls[key] as FormGroup;
-        for (const subKey in formGroup.controls) {
-          formGroup.controls[subKey].markAsTouched();
-        }
-      } else {
-        this.mainForm.controls[key].markAsTouched();
-      }
-    }
-  }
+
   onCountryChange(index: number) {
     const addressFormGroup = this.addresses.controls[index] as FormGroup;
+
     addressFormGroup.get('state')?.reset();
     addressFormGroup.get('city')?.reset();
   }
